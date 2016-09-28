0d5996cfde6565b7d1fdc0ff494f2015959845d5
Dorian Peake <dorian@nexmo.com>
2016-09-28 12:37:43 +0100

    updated to use go error handling style

--- nexmo.go
+++ nexmo.go
@@ -2,29 +2,64 @@ package main
 
 import (
     "fmt";
+    "flag";
     "os";
     //"net/http";
     "net/url";
     //"strings"
 )
 
+type ParamError string
+
+func (e ParamError) Error() string {
+    return string(e)
+}
+
 var baseUrl, _ = url.Parse("https://api.nexmo.com")
 
 func main() {
     if len(os.Args) <= 1 {
-        fmt.Println("Need an action kthxbai")
+        fmt.Println("Expecting action as first argument")
         os.Exit(1)
     }
 
     action := os.Args[1]
-    executeAction(action)
+
+    apiKey, key_err := parseParameter("apiKey", "NEXMO_API_KEY", "Nexmo API Key")
+
+    if len(key_err) != 0 {
+        fmt.Println("Failed to get Nexmo API Key: ", key_err)
+        os.Exit(1)
+    }
+
+    apiSecret, sec_err := parseParameter("apiSecret", "NEXMO_API_SECRET", "Nexmo API Secret")
+    if len(sec_err) != 0 {
+        fmt.Println("Failed to get Nexmo API Secret: " + sec_err)
+        os.Exit(1)
+    }
+
+    executeAction(action, apiKey, apiSecret)
 }
 
-func executeAction(action string) {
+func parseParameter(cmd_flag string, env_var string, description string) (param, err string) {
+    flag.StringVar(&param, cmd_flag, "", description)
+
+    if len(param) == 0 {
+        param = os.Getenv(env_var)
+
+        if len(param) == 0 {
+            err = "Could not find " + description + " in -" + cmd_flag + " parameter or " + env_var + " environment variable"
+            return
+        }
+    }
+
+    return
+}
+
+func executeAction(action, apiKey, apiSecret string) {
     switch action {
     case "list-applications":
-        fmt.Println("mkay will do list-applications")
-        fmt.Println("AHAHAHA JUST KID I DO NOTHING BYE!")
+        fmt.Println("will do list-applications")
         os.Exit(0)
 
     default:
