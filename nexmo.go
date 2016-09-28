a266c9e366ee57c8c02a5b1fc7c85f8f80df9140
Dorian Peake <dorian@nexmo.com>
2016-09-28 17:14:36 +0100

    Parse application list

--- nexmo.go
+++ nexmo.go
@@ -7,10 +7,27 @@ import (
     "net/http";
     "net/url";
     "io/ioutil";
+    "encoding/json";
     //"strings"
 )
 
 type ParamError string
+
+type Application struct {
+    Id string
+    Name string
+    Keys map[string]string
+}
+
+type ApplicationList struct {
+    Embedded struct {
+        Applications []Application
+    } `json:"_embedded"`
+}
+
+func (appList ApplicationList) Applications() []Application {
+    return appList.Embedded.Applications
+}
 
 func (e ParamError) Error() string {
     return string(e)
@@ -25,6 +42,11 @@ func main() {
         fmt.Println("Not all required arguments were found: ", err)
         os.Exit(2)
     }
+
+    v := url.Values{}
+    v.Add("api_key", apiKey)
+    v.Add("api_secret", apiSecret)
+    baseUrl.RawQuery = v.Encode()
 
     executeAction(action, apiKey, apiSecret)
 }
@@ -55,30 +77,43 @@ func parseCommandArgs() (action, apiKey, apiSecret string, err ParamError) {
 func executeAction(action, apiKey, apiSecret string) bool {
     switch action {
     case "list-applications":
-        applicationUrl := baseUrl
-        applicationUrl.Path = "v1/applications"
-        v := url.Values{}
-        v.Add("api_key", apiKey)
-        v.Add("api_secret", apiSecret)
-        applicationUrl.RawQuery = v.Encode()
-        fmt.Println(baseUrl.String(), applicationUrl.String())
-        resp, err := http.Get(applicationUrl.String())
-        if err != nil {
-            fmt.Println("Failed to list applications: ", err)
-            return false
-        }
-
-        bytes, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-            fmt.Println("Failed to read body of request: ", err)
-            return false
-        }
-
-        fmt.Println(string(bytes))
+        return listApplications()
 
     default:
         fmt.Println("Unknown action", action)
     }
 
     return false
+}
+
+func listApplications() bool {
+    applicationUrl := baseUrl
+    applicationUrl.Path = "v1/applications"
+
+    fmt.Println(baseUrl.String(), applicationUrl.String())
+    resp, err := http.Get(applicationUrl.String())
+    if err != nil {
+        fmt.Println("Failed to list applications: ", err)
+        return false
+    }
+
+    bytes, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        fmt.Println("Failed to read body of request: ", err)
+        return false
+    }
+
+    if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+        fmt .Println("Failed to perform http request. Failed with error: ", resp.Status)
+        return false
+    }
+
+    var appList ApplicationList
+    err = json.Unmarshal(bytes, &appList)
+
+    for _, app := range appList.Applications() {
+        fmt.Println(app.Id)
+    }
+
+    return true
 }
